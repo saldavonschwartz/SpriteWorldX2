0d9f4493cccf75e5b08062c319d19f7c90d1cb70
Federico Saldarini <saldavonschwartz@gmail.com>
2019-07-09 14:27:10 -0700

    fixed: cmake unmounts for sdl libs.

--- cmake/FindSDL2.cmake
+++ cmake/FindSDL2.cmake
@@ -1,8 +1,8 @@
 if (SDL2_PATH)
     message("[USING SDL2 AT: ${SDL2_PATH}]")
     if(APPLE)
-        set(SDL2_INCLUDE_DIR ${SDL2_PATH}/SDL2.framework/Headers) 
-        set(SDL2_LIB ${SDL2_PATH}/SDL2.framework) 
+        set(SDL2_INCLUDE_DIR ${SDL2_PATH}/SDL2.framework/Headers)
+        set(SDL2_LIB ${SDL2_PATH}/SDL2.framework)
     endif()
 else()
     set(SDL2_PATH ${CMAKE_BINARY_DIR}/ext/sdl2 CACHE STRING "SDL2 lib/include root path")
@@ -14,26 +14,26 @@ else()
         ${CMAKE_BINARY_DIR}/ext/temp.dmg
         SHOW_PROGRESS)
 
-        execute_process(COMMAND 
-        hdiutil attach -mountpoint temp temp.dmg
+        execute_process(COMMAND
+        hdiutil attach temp.dmg
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/ext/)
 
-        execute_process(COMMAND 
+        execute_process(COMMAND
         mkdir sdl2
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/ext/)
 
         file(GLOB TEMP_FILES ${CMAKE_BINARY_DIR}/ext/temp/*)
         file(COPY ${TEMP_FILES} DESTINATION ${CMAKE_BINARY_DIR}/ext/sdl2)
-        
-        execute_process(COMMAND 
-        hdiutil unmount temp -force
+
+        execute_process(COMMAND
+        hdiutil detach /Volumes/temp
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/ext/)
 
-        execute_process(COMMAND 
+        execute_process(COMMAND
         rm temp.dmg
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/ext/)
-        
-        set(SDL2_INCLUDE_DIR ${SDL2_PATH}/SDL2.framework/Headers) 
-        set(SDL2_LIB ${SDL2_PATH}/SDL2.framework) 
+
+        set(SDL2_INCLUDE_DIR ${SDL2_PATH}/SDL2.framework/Headers)
+        set(SDL2_LIB ${SDL2_PATH}/SDL2.framework)
     endif()
 endif()
